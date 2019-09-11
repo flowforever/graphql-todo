28bec489b06ab89122dc52fd684581945e22880e
trump.wang <trump.wang@ringcentral.com>
2019-09-11 20:35:41 +0800

    implement apollo  graphql server

--- server/app.js
+++ server/app.js
@@ -4,10 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const apolloServer = require('./gql/server');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+apolloServer.applyMiddleware({app});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
